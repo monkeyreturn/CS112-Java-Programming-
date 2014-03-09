389bb295c913555f35bc225f8dd85bc6911ffc65
Jim Nguyen <monkeyreturn@yahoo.com>
2014-03-08 19:40:31 -0800

    Homework4

---
+++ HomeWork4/src/HomeWork4.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class HomeWork4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner (System.in);
+		int number1;
+		int number2;
+		
+		System.out.print("Enter first number: ");
+		number1 = input.nextInt();
+		
+		System.out.print("Enter second number: ");
+		number2 = input.nextInt();
+		
+		System.out.printf("Hello, you enter %d and %d", number1, number2);
+		
+	}
+
+}
