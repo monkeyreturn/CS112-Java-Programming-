c3dc8bf5bcbeb9656b108cafd934005f91418bb7
Jim Nguyen <monkeyreturn@yahoo.com>
2014-04-17 20:41:14 -0700

    Week 9 Lab 2
    Random number game with methods

---
+++ Random/src/randomTest.java
@@ -0,0 +1,17 @@
+
+public class randomTest {
+	public static int GetANumber(int high){
+	
+	
+		return high;
+		
+	}
+	
+		public static int GetANumber_between_1_and_10(int high){
+		     
+		     int B = 0+(int)(Math.random()*GetANumber(high));
+			//System.out.println(number);
+			return B;
+		}
+		
+}
